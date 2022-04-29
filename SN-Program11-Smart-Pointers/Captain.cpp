55443f6e1e62931b58dbc6a7c34c1e74895e0424
IlliterateElf <illiterate.elf@outlook.com>
2022-04-28 18:24:21 -0700

    Captian.cpp done

--- SN-Program11-Smart-Pointers/Captain.cpp
+++ SN-Program11-Smart-Pointers/Captain.cpp
@@ -1,5 +1,14 @@
 #include "Captain.h"
 
 bool Captain::BoatsHere() {
-
+	bool result;
+	std::list<std::weak_ptr<Lifeboat>>::iterator it;
+	for (it = mSightedBoats.begin(); it != mSightedBoats.end(); it++)
+	{
+		if (it != mSightedBoats.end()) //If a boat exists
+		{
+			return true;
+		}
+	}
+	return false;
 }
